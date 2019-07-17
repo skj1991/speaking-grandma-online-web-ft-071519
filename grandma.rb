aa2d48dfc5c5c09a5a5693311c644dc581f51736
Stacy Johns <stacykjohns@icloud.com>
2019-07-17 00:07:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(string)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif string.upcase
     return "NO, NOT SINCE 1938!"
-  elsif string = string.capitalize
+  elsif string = string.capitalize || string.downcase
     return "HUH?! SPEAK UP, SONNY!"
   end
 end

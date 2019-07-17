7e172e28461bd8e9fdaf5da5a11ecb3b8838c97a
Stacy Johns <stacykjohns@icloud.com>
2019-07-17 00:10:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif string.upcase
-    return "NO, NOT SINCE 1938!"
-  else string != string.upcase
+  elsif string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
+  else string.upcase
+    return "NO, NOT SINCE 1938!"
   end
 end
